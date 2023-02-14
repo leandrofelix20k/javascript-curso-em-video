79e5bae54789f97ccbe220a44e73011c53ddf6bd
Leandro Felix <leandro.almeida3029@gmail.com>
2023-02-14 10:13:14 -0300

    Create anotacoes.txt and anotacoes.js

---
+++ anotacoes/modulo-b/anotações.js
@@ -0,0 +1,24 @@
+// Aula 5 - Variáveis e Tipos Primitivos 
+
+    var num = 7
+
+    // Três formas de se declarar uma string
+    var text1 = 'Primeira forma'
+    var text2 = "Segunda"
+    var text3 = `Terceira`
+
+    // Identificadores:
+    var $cifrao = '$'
+    var _sublinhado = '_'
+
+    var n1 = 9
+    // Exibir o tipo da variável:
+    console.log(typeof n1) // number
+
+    /* 
+    Em JavaScript, ao definir a variável, é reservado um espaço para 
+    essa variável, é possível modificar tanto o valor como o seu tipo 
+    primitivo
+    */
+    n1 = 'Leandro'
+    console.log(typeof n1) //string
