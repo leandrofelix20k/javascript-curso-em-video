7649adee7cb609afd253c3fddde0922a298cf518
Leandro Felix <104033917+leandrofelix20k@users.noreply.github.com>
2023-06-21 12:06:02 -0300

    Create modulo-d and ex008

---
+++ anotacoes/modulo-d/anotacoes.js
@@ -0,0 +1,12 @@
+// Aula 11 e 12 – Condições
+    console.log('Aula 11')
+
+    let num = 3
+    // Estrutura
+        if (num % 2 == 0){
+            console.log(`${num} é par!`)
+        } else{
+            console.log(`${num} é ìmpar!`)
+        }
+    
+    
